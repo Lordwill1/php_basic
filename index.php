60b62de9dde3943673c7c3287ff1a78541e1122f
Lordwill1 <messagelordwill@gmail.com>
2024-03-24 13:07:45 +0100

    Global variable

--- index.php
+++ index.php
@@ -10,6 +10,10 @@
     echo $_SERVER ["DOCUMENT_ROOT"];
     echo "<br>";
     echo $_SERVER ["PHP_SELF"];
+    echo "<br>";
+    echo $_SERVER ["SERVER_NAME"];
+    echo "<br>";
+    echo $_SERVER ["REQUEST_METHOD"];
     ?>
     
 
