869f9d90ffe4abb64c3e513a86e91bb4250c29b3
Lordwill1 <messagelordwill@gmail.com>
2024-03-26 21:02:29 +0100

    html style

--- index.php
+++ index.php
@@ -4,16 +4,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Web Page</title>
 </head>
-<body>
+<body style="text-align:center;">
 
     <main>
         <form action="formhandler.php" method="POST">
-            <label for="firstname">First Name</label>
+            <label style="color:blue;" title="Enter your name"  for="firstname"><b>First Name</b></label>
             <input required id="firstname" type="text" name="firstname" placeholder="First Name...">
 
             <br><br>
 
-            <label for="lastname">Last Name</label>
+            <label style="border:2px solid blue;" for="lastname"> Last Name </label>
             <input id="lastname" type="text" name="lastname" placeholder="Last Name...">
 
             <br><br>
@@ -28,6 +28,7 @@
             </select>
 
             <br>
+            <hr>
 
             <button type="submit">Submit</button>
         </form>
